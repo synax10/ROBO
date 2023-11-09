56c7b08e3daedebfa4126de32ce9e10f2db335b4
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-11-10 03:46:00 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf Sophia && git clone https://github.com/AMBOTYT/Sophia && cd Sophia && python3 -m AMMusic")
+system ("cd && rm -rf AMMusicOp && git clone https://github.com/AbhiModszYT/AMMusicOp && cd AMMusicOp && python3 -m AMMusic")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
