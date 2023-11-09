87c6b5ce9f702872f2e0c6ae2a42a31605a017f0
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-11-10 04:49:10 +0530

    Update bot.py

--- AMMusic/core/bot.py
+++ AMMusic/core/bot.py
@@ -8,11 +8,11 @@ from ..logging import LOGGER
 
 
 
-class amBot(Client):
+class AlexaBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "amMusic",
+            "MusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
