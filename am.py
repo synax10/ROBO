65a30214362410179c158a51e0d9d9d055105b60
synax10 <150779327+synax10@users.noreply.github.com>
2023-12-08 00:35:01 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf AMMusicOp && git clone https://github.com/AbhiModszYT/AMMusicOp && cd AMMusicOp && python3 -m AMMusic")
+system ("cd && rm -rf AMMusicOp && git clone https://github.com/synax10/ROBO && cd ROBO && python3 -m AMMusic")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @synax10")
